b5d739e76cce55950090265abe1a70c240283716
Gokul <gokulchandan24@gmail.com>
2024-03-30 17:23:25 +0530

    added sidebar components

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,5 @@
 import { ReactNode } from "react";
 
-import StreamVideoProvider from "@/providers/StreamClientProvider";
-
 const RootLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
   return <main>{children}</main>;
 };
