88b9468a312ad28d305422b95bd97c65c876a4c6
Gokul <gokulchandan24@gmail.com>
2024-03-31 19:50:50 +0530

    usestate used

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -2,9 +2,8 @@
 "use client";
 
 import HomeCard from "./HomeCard";
-
-// import { useState } from 'react';
-// import { useRouter } from 'next/navigation';
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 // import MeetingModal from './MeetingModal';
 // import { Call, useStreamVideoClient } from '@stream-io/video-react-sdk';
@@ -180,6 +179,11 @@ import HomeCard from "./HomeCard";
 // export default MeetingTypeList;
 
 const MeetingTypeList = () => {
+  const router = useRouter();
+
+  const [meetingState, setMeetingState] = useState<
+    "isScheduleMeeting" | "isJoiningMeeting" | "isInstantMeeting" | undefined
+  >(undefined);
   return (
     <>
       <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
@@ -187,28 +191,28 @@ const MeetingTypeList = () => {
           img="/icons/add-meeting.svg"
           title="New Meeting"
           description="Start an instant meeting"
-          // handleClick={() => setMeetingState('isInstantMeeting')}
+          handleClick={() => setMeetingState("isInstantMeeting")}
         />
         <HomeCard
           img="/icons/join-meeting.svg"
           title="Join Meeting"
           description="via invitation link"
           className="bg-blue-1"
-          // handleClick={() => setMeetingState('isJoiningMeeting')}
+          handleClick={() => setMeetingState("isJoiningMeeting")}
         />
         <HomeCard
           img="/icons/schedule.svg"
           title="Schedule Meeting"
           description="Plan your meeting"
           className="bg-purple-1"
-          // handleClick={() => setMeetingState('isScheduleMeeting')}
+          handleClick={() => setMeetingState("isScheduleMeeting")}
         />
         <HomeCard
           img="/icons/recordings.svg"
           title="View Recordings"
           description="Meeting Recordings"
           className="bg-yellow-1"
-          // handleClick={() => router.push('/recordings')}
+          handleClick={() => router.push("/recordings")}
         />
       </section>
     </>
